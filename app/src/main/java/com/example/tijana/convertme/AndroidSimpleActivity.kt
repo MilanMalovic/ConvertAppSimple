1696c3259d3802048216cfa34db178669c0b6f76
mm2308 <milan.malovic@yahoo.com>
2018-12-05 21:23:13 +0100

    first

---
+++ app/src/main/java/com/example/tijana/convertme/AndroidSimpleActivity.kt
@@ -0,0 +1,57 @@
+package com.example.tijana.convertme
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+
+import kotlinx.android.synthetic.main.activity_android_simple.*
+import kotlinx.android.synthetic.main.content_android_simple.*
+
+class AndroidSimpleActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android_simple)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    fun convertCurrency(view: View) {
+        if (dollarText.text.isNotEmpty()) {
+            val dollarValue = dollarText.text.toString().toFloat()
+            val euroValue = dollarValue * 0.85f
+
+            textView.text = euroValue.toString()
+
+        }else{
+            textView.text = "No value"
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_android_simple, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
